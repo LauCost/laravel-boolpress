b5feb81c65989e1478f7cfc09f1d6d48bbf4c5c9
Laurentiu <lcostin40@gmail.com>
2022-01-26 20:15:46 +0100

    Aggiungo gruppi di route, admin files e guest files

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,15 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('guest.welcome');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::namespace ('Admin')->prefix('admin')->name('admin.')->middleware('auth')->group(function () {
+    Route::get('/', 'HomeController@index')->name('dashboard');
+
+});
